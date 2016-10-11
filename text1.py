ae91b87ff3c2252d02b786fe20a3fa531a207dd0
ladinmbdn@gmail.com <ladinmbdn@gmail.com>
2016-10-12 00:14:43 +0800

    文件读写练习，#一个有2000行文本文件每隔1000行写道一个新的文件中

--- text1.py
+++ text1.py
@@ -2,7 +2,7 @@
 #一个有2000行文本文件每隔1000行写道一个新的文件中
 r1 = open('all.txt','r')    #读取文件 
 i = 0                       #计数器
-f = open('001'+1,'w+')   #打开并查看是否有文件没有新建
+f = open('001.txt','w+')   #打开并查看是否有文件没有新建
 f1 = open ('002.txt','w+')
 for line in r1:             #把每一行给line
     i += 1
@@ -11,3 +11,7 @@ for line in r1:             #把每一行给line
         f.write(line)
     elif i <= 2000:
         f1.write(line)
+
+r1.close()
+f.close()
+f1.close()
