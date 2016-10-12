8edcbd386de0ad6a60fe840ffe024994605cacd5
ladinmbdn@gmail.com <ladinmbdn@gmail.com>
2016-10-12 12:29:44 +0800

    有一个nM的文件，每隔1000行写道一个新文件中

--- text1.py
+++ text1.py
@@ -1,17 +1,17 @@
 #coding:utf-8
 #一个有2000行文本文件每隔1000行写道一个新的文件中
 r1 = open('all.txt','r')    #读取文件 
-i = 0                       #计数器
-f = open('001.txt','w+')   #打开并查看是否有文件没有新建
-f1 = open ('002.txt','w+')
+i = 0   #计数器
+ii = -1                     #
+
 for line in r1:             #把每一行给line
     i += 1
     #rr.write(line)          #写数据
-    if i <= 1000:       #检查是否到1000行
-        f.write(line)
-    elif i <= 2000:
-        f1.write(line)
+    if i % 10000 == 0:
+        ii += 1
+    f = open(str(ii)+'.txt','a')
+    f.write(line)
+
 
 r1.close()
-f.close()
-f1.close()
+
