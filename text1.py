e13f5c37b4a1cb8bd38e20c4d94d91a4317e28cf
ladinmbdn@gmail.com <ladinmbdn@gmail.com>
2016-10-12 12:58:19 +0800

    一个有2000行文本文件每隔1000行写道一个新的文件中

--- text1.py
+++ text1.py
@@ -1,17 +1,13 @@
 #coding:utf-8
-#一个有2000行文本文件每隔1000行写道一个新的文件中
+#一个有nM的文本文件，每隔1000行写道一个新的文件中
 r1 = open('all.txt','r')    #读取文件 
-i = 0   #计数器
-ii = -1                     #
-
+i = -1   #计数器
+ii = 0                     #文件名
 for line in r1:             #把每一行给line
     i += 1
-    #rr.write(line)          #写数据
-    if i % 10000 == 0:
+    if i % 10 == 0:
         ii += 1
-    f = open(str(ii)+'.txt','a')
-    f.write(line)
-
-
-r1.close()
+    f = open(str(ii)+'.txt','a')    #打开要写入的文件
+    f.write(line)                   #写入数据到文件
+r1.close()                          
 
