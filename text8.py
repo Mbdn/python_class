3bcfd3ad8d8fb0b0f9c35e6a11345931edcb91f1
ladinmbdn@gmail.com <ladinmbdn@gmail.com>
2016-10-19 23:51:46 +0800

    九九乘法表

--- text8.py
+++ text8.py
@@ -2,7 +2,7 @@
 #九九口诀
 import math
 if __name__ == "__main__":
-    i, j = range(1,10),range(1,10)
+    i = range(1,10)
     for a in i:
         for b  in range(1,a + 1):
             print "%s * %s = %s" % (a,b,a*b),
