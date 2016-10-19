c532ae6076a11f524a12cea0a724ebe9637d3515
ladinmbdn@gmail.com <ladinmbdn@gmail.com>
2016-10-19 23:50:53 +0800

    九九乘法表

---
+++ text8.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+#九九口诀
+import math
+if __name__ == "__main__":
+    i, j = range(1,10),range(1,10)
+    for a in i:
+        for b  in range(1,a + 1):
+            print "%s * %s = %s" % (a,b,a*b),
+        print 
